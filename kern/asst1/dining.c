b4f440677974b30e468fdd733caab547804ba859
Joshua Long <z5152065@student.unsw.edu.au>
2019-03-03 19:34:32 +1100

    A1 Q4 upload

--- kern/asst1/dining.c
+++ kern/asst1/dining.c
@@ -12,8 +12,15 @@
  * your forks here.
  */
 
+#define LEFT(d) (phil_num+NUM_PHILOSOPHERS-d)%NUM_PHILOSOPHERS
+#define RIGHT(d) (phil_num+d)%NUM_PHILOSOPHERS
 
+/* semaphore declaration */
+static struct semaphore *mutex;
+static struct semaphore **philosopher_semaphores;
 
+/* declare state array (T = Thinking, H = Hungry, E = Eating) */
+char * state;
 
 /*
  * Take forks ensures mutually exclusive access to two forks
@@ -25,7 +32,15 @@
 
 void take_forks(unsigned long phil_num)
 {
-    (void) phil_num;
+    P(mutex);
+    state[phil_num] = 'H';
+    /* test whether the current philosopher can enter the eating state */
+    if (state[phil_num] == 'H' && state[LEFT(1)] != 'E' && state[RIGHT(1)] != 'E') {
+        state[phil_num] = 'E';
+        V(philosopher_semaphores[phil_num]);
+    }
+    V(mutex);
+    P(philosopher_semaphores[phil_num]);
 }
 
 
@@ -36,7 +51,20 @@ void take_forks(unsigned long phil_num)
 
 void put_forks(unsigned long phil_num)
 {
-    (void) phil_num;
+    P(mutex);
+    state[phil_num] = 'T';
+
+    /* test whether the left philosopher can enter the eating state */
+    if (state[LEFT(1)] == 'H' && state[LEFT(2)] != 'E' && state[phil_num] != 'E') {
+        state[LEFT(1)] = 'E';
+        V(philosopher_semaphores[LEFT(1)]);
+    }
+    /* test whether the right philosopher can enter the eating state */
+    if (state[RIGHT(1)] == 'H' && state[RIGHT(2)] != 'E' && state[phil_num] != 'E') {
+        state[RIGHT(1)] = 'E';
+        V(philosopher_semaphores[RIGHT(1)]);
+    }
+    V(mutex);
 }
 
 
@@ -47,7 +75,21 @@ void put_forks(unsigned long phil_num)
 
 void create_forks()
 {
+    /* allocate memory for arrays */
+    philosopher_semaphores = kmalloc(sizeof(struct semaphore *) * NUM_PHILOSOPHERS);
+    if (philosopher_semaphores == NULL)
+        panic("Cannot kmalloc philosopher_semaphores array.");
+    state = kmalloc(sizeof(char) * NUM_PHILOSOPHERS);
+    if (state == NULL)
+        panic("Cannot kmalloc state array.");
 
+    /* create semaphores */
+    mutex = sem_create("mutex", 1);
+    int phil_num;
+    for (phil_num = 0; phil_num < NUM_PHILOSOPHERS; phil_num++)
+        philosopher_semaphores[phil_num] = sem_create("philosopher", 0);
+        /* initialise each philosopher to state T (thinking) */
+        state[phil_num] = 'T';
 }
 
 
@@ -58,5 +100,13 @@ void create_forks()
 
 void destroy_forks()
 {
-    
+    /* destory all semaphores */
+    sem_destroy(mutex);
+    int i;
+    for (i = 0; i < NUM_PHILOSOPHERS; i++)
+        sem_destroy(philosopher_semaphores[i]);
+
+    /* free allocated memory for variables */
+    kfree(philosopher_semaphores);
+    kfree(state);
 }
